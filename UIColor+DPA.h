5de4a51838bc8b339af895789615efc08885d42f
dapeng <799825870@qq.com>
2019-12-17 15:54:58 +0800

    gengixn

---
+++ UIColor+DPA.h
@@ -0,0 +1,19 @@
+//
+//  UIColor+DPA.h
+//  ModuleDapengB
+//
+//  Created by dapeng on 2019/12/17.
+//
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIColor (DPA)
+
++(UIColor *)randomColor;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UIColor+DPA.m
@@ -0,0 +1,14 @@
+//
+//  UIColor+DPA.m
+//  ModuleDapengB
+//
+//  Created by dapeng on 2019/12/17.
+//
+
+#import "UIColor+DPA.h"
+
+@implementation UIColor (DPA)
++(UIColor *)randomColor{
+    return [UIColor yellowColor];
+}
+@end
